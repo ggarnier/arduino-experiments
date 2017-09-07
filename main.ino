e9cc09e4f1d647569ba87246e742faf58e52f9d2
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-09-07 10:40:13 -0300

    Add red light

--- main.ino
+++ main.ino
@@ -1,16 +1,22 @@
 const int green = 12;
-const int yellow =10;
+const int yellow = 10;
+const int red = 7;
 
 void setup() {
   pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
+  pinMode(red, OUTPUT);
 }
 
 void loop() {
   digitalWrite(green, HIGH);
-  digitalWrite(yellow, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
   delay(3000);
   digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(3000);
   digitalWrite(yellow, LOW);
-  delay(1000);
+  digitalWrite(red, HIGH);
+  delay(3000);
 }
